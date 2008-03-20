75c431d6b070380b3c88bf06ab36140b62dc29e1
jomora <jomora@1ae2de4a-3c8b-9346-81e6-8f1271a8d7bb>
2008-03-20 16:40:58 +0000

    svnlogファイル名を短縮

--- repos/post-commit.bat
+++ repos/post-commit.bat
@@ -5,6 +5,6 @@ SET REVISON=%2
 
 IF "%REPOS_NAME%" == "" EXIT 1
 
-SET XML_FILENAME=D:\Inetpub\apache_htdocs\svnlog\svnlog_%REPOS_NAME%.xml
+SET XML_FILENAME=D:\Inetpub\apache_htdocs\svnlog\%REPOS_NAME%.xml
 
 "C:\Program Files\Subversion\bin\svn.exe" log http://jomora.net/svn/%REPOS_NAME%/ --xml --limit 30 --verbose > %XML_FILENAME%
