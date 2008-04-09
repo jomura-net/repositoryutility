beede31e6938bea5c044f5fd8fc2cc91fefd0187
jomora <jomora@1ae2de4a-3c8b-9346-81e6-8f1271a8d7bb>
2008-04-09 12:35:42 +0000

    一日前日付の計算バグを改修

--- vss/show_vss_history_of_yesterday.bat
+++ vss/show_vss_history_of_yesterday.bat
@@ -1,18 +1,19 @@
 @echo off
 
-rem 1“ú‘O‚Ì“ú•t‚ðŒvŽZ‚·‚é
-
+:: Today
 set yy=%date:~0,4%
 set mm=%date:~5,2%
 set dd=%date:~8,2%
 
-set /a dd=dd-1
+:: Yesterday
+if %dd:~0,1%==0 set dd=%dd:~1,1%
+set /a dd=%dd%-1
 set dd=00%dd%
 set dd=%dd:~-2%
 set /a ymod=yy %% 4
 
 if %dd%==00 (
-if %mm%==01 (set mm=12&& set dd=31&& set /a yy=yy-1)
+if %mm%==01 (set mm=12&& set dd=31&& set /a yy=%yy%-1)
 if %mm%==02 (set mm=01&& set dd=31)
 if %mm%==03 (set mm=02&& set dd=28&& if %ymod%==0 (set dd=29))
 if %mm%==04 (set mm=03&& set dd=31)
@@ -29,12 +30,12 @@ if %mm%==12 (set mm=11&& set dd=30)
 set yesterday=%yy%/%mm%/%dd%
 set yesterday_yymmdd=%yy%%mm%%dd%
 
-rem ŠÂ‹«•Ï”‚ÌÝ’è
+rem SourceSafe Environment Variable
 set SSDIR=\\10.130.208.37\vssdata\CITY-2e_1.1.1
 set ssUser=build
 set ssPwd=
 
-rem ‚P“ú‘O‚Ì—š—ð
-"C:\Program Files\Microsoft Visual SourceSafe\2005\ss.exe" History $/@Head -R -Vd%yesterday%;23:59~%yesterday%;00:00 > vss_history_%yesterday_yymmdd%.txt
+rem Get History
+"C:\Program Files\Microsoft Visual SourceSafe\ss.exe" History $/@Head -R -Vd%yesterday%;23:59~%yesterday%;00:00 > vss_history_%yesterday_yymmdd%.txt
 
 echo --end  >> vss_history_%yesterday_yymmdd%.txt
